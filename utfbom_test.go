f6dac75f157494fa2e974a686ab36f9f5f0c5955
Tiago Peczenyj <tpeczenyj@weborama.com>
2023-09-17 13:36:47 +0200

    change test package to add _test suffix

--- utfbom_test.go
+++ utfbom_test.go
@@ -1,4 +1,4 @@
-package utfbom
+package utfbom_test
 
 import (
 	"io"
@@ -6,48 +6,50 @@ import (
 	"testing"
 	"testing/iotest"
 	"time"
+
+	"github.com/dimchansky/utfbom"
 )
 
 var testCases = []struct {
 	name       string
 	input      []byte
 	inputError error
-	encoding   Encoding
+	encoding   utfbom.Encoding
 	output     []byte
 }{
-	{"1", []byte{}, nil, Unknown, []byte{}},
-	{"2", []byte("hello"), nil, Unknown, []byte("hello")},
-	{"3", []byte("\xEF\xBB\xBF"), nil, UTF8, []byte{}},
-	{"4", []byte("\xEF\xBB\xBFhello"), nil, UTF8, []byte("hello")},
-	{"5", []byte("\xFE\xFF"), nil, UTF16BigEndian, []byte{}},
-	{"6", []byte("\xFF\xFE"), nil, UTF16LittleEndian, []byte{}},
-	{"7", []byte("\x00\x00\xFE\xFF"), nil, UTF32BigEndian, []byte{}},
-	{"8", []byte("\xFF\xFE\x00\x00"), nil, UTF32LittleEndian, []byte{}},
+	{"1", []byte{}, nil, utfbom.Unknown, []byte{}},
+	{"2", []byte("hello"), nil, utfbom.Unknown, []byte("hello")},
+	{"3", []byte("\xEF\xBB\xBF"), nil, utfbom.UTF8, []byte{}},
+	{"4", []byte("\xEF\xBB\xBFhello"), nil, utfbom.UTF8, []byte("hello")},
+	{"5", []byte("\xFE\xFF"), nil, utfbom.UTF16BigEndian, []byte{}},
+	{"6", []byte("\xFF\xFE"), nil, utfbom.UTF16LittleEndian, []byte{}},
+	{"7", []byte("\x00\x00\xFE\xFF"), nil, utfbom.UTF32BigEndian, []byte{}},
+	{"8", []byte("\xFF\xFE\x00\x00"), nil, utfbom.UTF32LittleEndian, []byte{}},
 	{"5", []byte("\xFE\xFF\x00\x68\x00\x65\x00\x6C\x00\x6C\x00\x6F"), nil,
-		UTF16BigEndian, []byte{0x00, 0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F}},
+		utfbom.UTF16BigEndian, []byte{0x00, 0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F}},
 	{"6", []byte("\xFF\xFE\x68\x00\x65\x00\x6C\x00\x6C\x00\x6F\x00"), nil,
-		UTF16LittleEndian, []byte{0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F, 0x00}},
+		utfbom.UTF16LittleEndian, []byte{0x68, 0x00, 0x65, 0x00, 0x6C, 0x00, 0x6C, 0x00, 0x6F, 0x00}},
 	{"7", []byte("\x00\x00\xFE\xFF\x00\x00\x00\x68\x00\x00\x00\x65\x00\x00\x00\x6C\x00\x00\x00\x6C\x00\x00\x00\x6F"), nil,
-		UTF32BigEndian,
+		utfbom.UTF32BigEndian,
 		[]byte{0x00, 0x00, 0x00, 0x68, 0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x6F}},
 	{"8", []byte("\xFF\xFE\x00\x00\x68\x00\x00\x00\x65\x00\x00\x00\x6C\x00\x00\x00\x6C\x00\x00\x00\x6F\x00\x00\x00"), nil,
-		UTF32LittleEndian,
+		utfbom.UTF32LittleEndian,
 		[]byte{0x68, 0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x6F, 0x00, 0x00, 0x00}},
-	{"9", []byte("\xEF"), nil, Unknown, []byte("\xEF")},
-	{"10", []byte("\xEF\xBB"), nil, Unknown, []byte("\xEF\xBB")},
-	{"11", []byte("\xEF\xBB\xBF"), io.ErrClosedPipe, UTF8, []byte{}},
-	{"12", []byte("\xFE\xFF"), io.ErrClosedPipe, Unknown, []byte("\xFE\xFF")},
-	{"13", []byte("\xFE"), io.ErrClosedPipe, Unknown, []byte("\xFE")},
-	{"14", []byte("\xFF\xFE"), io.ErrClosedPipe, Unknown, []byte("\xFF\xFE")},
-	{"15", []byte("\x00\x00\xFE\xFF"), io.ErrClosedPipe, UTF32BigEndian, []byte{}},
-	{"16", []byte("\x00\x00\xFE"), io.ErrClosedPipe, Unknown, []byte{0x00, 0x00, 0xFE}},
-	{"17", []byte("\x00\x00"), io.ErrClosedPipe, Unknown, []byte{0x00, 0x00}},
-	{"18", []byte("\x00"), io.ErrClosedPipe, Unknown, []byte{0x00}},
-	{"19", []byte("\xFF\xFE\x00\x00"), io.ErrClosedPipe, UTF32LittleEndian, []byte{}},
-	{"20", []byte("\xFF\xFE\x00"), io.ErrClosedPipe, Unknown, []byte{0xFF, 0xFE, 0x00}},
-	{"21", []byte("\xFF\xFE"), io.ErrClosedPipe, Unknown, []byte{0xFF, 0xFE}},
-	{"22", []byte("\xFF"), io.ErrClosedPipe, Unknown, []byte{0xFF}},
-	{"23", []byte("\x68\x65"), nil, Unknown, []byte{0x68, 0x65}},
+	{"9", []byte("\xEF"), nil, utfbom.Unknown, []byte("\xEF")},
+	{"10", []byte("\xEF\xBB"), nil, utfbom.Unknown, []byte("\xEF\xBB")},
+	{"11", []byte("\xEF\xBB\xBF"), io.ErrClosedPipe, utfbom.UTF8, []byte{}},
+	{"12", []byte("\xFE\xFF"), io.ErrClosedPipe, utfbom.Unknown, []byte("\xFE\xFF")},
+	{"13", []byte("\xFE"), io.ErrClosedPipe, utfbom.Unknown, []byte("\xFE")},
+	{"14", []byte("\xFF\xFE"), io.ErrClosedPipe, utfbom.Unknown, []byte("\xFF\xFE")},
+	{"15", []byte("\x00\x00\xFE\xFF"), io.ErrClosedPipe, utfbom.UTF32BigEndian, []byte{}},
+	{"16", []byte("\x00\x00\xFE"), io.ErrClosedPipe, utfbom.Unknown, []byte{0x00, 0x00, 0xFE}},
+	{"17", []byte("\x00\x00"), io.ErrClosedPipe, utfbom.Unknown, []byte{0x00, 0x00}},
+	{"18", []byte("\x00"), io.ErrClosedPipe, utfbom.Unknown, []byte{0x00}},
+	{"19", []byte("\xFF\xFE\x00\x00"), io.ErrClosedPipe, utfbom.UTF32LittleEndian, []byte{}},
+	{"20", []byte("\xFF\xFE\x00"), io.ErrClosedPipe, utfbom.Unknown, []byte{0xFF, 0xFE, 0x00}},
+	{"21", []byte("\xFF\xFE"), io.ErrClosedPipe, utfbom.Unknown, []byte{0xFF, 0xFE}},
+	{"22", []byte("\xFF"), io.ErrClosedPipe, utfbom.Unknown, []byte{0xFF}},
+	{"23", []byte("\x68\x65"), nil, utfbom.Unknown, []byte{0x68, 0x65}},
 }
 
 type sliceReader struct {
@@ -94,7 +96,7 @@ func TestSkip(t *testing.T) {
 		for _, readMaker := range readMakers {
 			r := readMaker.fn(&sliceReader{tc.input, tc.inputError})
 
-			sr, enc := Skip(r)
+			sr, enc := utfbom.Skip(r)
 			if enc != tc.encoding {
 				t.Fatalf("test %v reader=%s: expected encoding %v, but got %v", tc.name, readMaker.name, tc.encoding, enc)
 			}
@@ -115,10 +117,10 @@ func TestSkipSkip(t *testing.T) {
 		for _, readMaker := range readMakers {
 			r := readMaker.fn(&sliceReader{tc.input, tc.inputError})
 
-			sr0, _ := Skip(r)
-			sr, enc := Skip(sr0)
-			if enc != Unknown {
-				t.Fatalf("test %v reader=%s: expected encoding %v, but got %v", tc.name, readMaker.name, Unknown, enc)
+			sr0, _ := utfbom.Skip(r)
+			sr, enc := utfbom.Skip(sr0)
+			if enc != utfbom.Unknown {
+				t.Fatalf("test %v reader=%s: expected encoding %v, but got %v", tc.name, readMaker.name, utfbom.Unknown, enc)
 			}
 
 			output, err := io.ReadAll(sr)
@@ -137,7 +139,7 @@ func TestSkipOnly(t *testing.T) {
 		for _, readMaker := range readMakers {
 			r := readMaker.fn(&sliceReader{tc.input, tc.inputError})
 
-			sr := SkipOnly(r)
+			sr := utfbom.SkipOnly(r)
 
 			output, err := io.ReadAll(sr)
 			if !reflect.DeepEqual(output, tc.output) {
@@ -157,8 +159,8 @@ func (zeroReader) Read(p []byte) (int, error) {
 }
 
 type readerEncoding struct {
-	Rd  *Reader
-	Enc Encoding
+	Rd  *utfbom.Reader
+	Enc utfbom.Encoding
 }
 
 func TestSkipZeroReader(t *testing.T) {
@@ -166,14 +168,14 @@ func TestSkipZeroReader(t *testing.T) {
 
 	c := make(chan readerEncoding)
 	go func() {
-		r, enc := Skip(z)
+		r, enc := utfbom.Skip(z)
 		c <- readerEncoding{r, enc}
 	}()
 
 	select {
 	case re := <-c:
-		if re.Enc != Unknown {
-			t.Error("Unknown encoding expected")
+		if re.Enc != utfbom.Unknown {
+			t.Error("utfbom.Unknown encoding expected")
 		} else {
 			var b [1]byte
 			n, err := re.Rd.Read(b[:])
@@ -185,16 +187,16 @@ func TestSkipZeroReader(t *testing.T) {
 			}
 		}
 	case <-time.After(time.Second):
-		t.Error("test timed out (endless loop in Skip?)")
+		t.Error("test timed out (endless loop in utfbom.Skip?)")
 	}
 }
 
 func TestSkipOnlyZeroReader(t *testing.T) {
 	var z zeroReader
 
-	c := make(chan *Reader)
+	c := make(chan *utfbom.Reader)
 	go func() {
-		r := SkipOnly(z)
+		r := utfbom.SkipOnly(z)
 		c <- r
 	}()
 
@@ -209,7 +211,7 @@ func TestSkipOnlyZeroReader(t *testing.T) {
 			t.Error("unexpected error:", err)
 		}
 	case <-time.After(time.Second):
-		t.Error("test timed out (endless loop in Skip?)")
+		t.Error("test timed out (endless loop in utfbom.Skip?)")
 	}
 }
 
@@ -218,7 +220,7 @@ func TestReader_ReadEmpty(t *testing.T) {
 		for _, readMaker := range readMakers {
 			r := readMaker.fn(&sliceReader{tc.input, tc.inputError})
 
-			sr := SkipOnly(r)
+			sr := utfbom.SkipOnly(r)
 
 			n, err := sr.Read(nil)
 			if n != 0 {
@@ -232,13 +234,13 @@ func TestReader_ReadEmpty(t *testing.T) {
 }
 
 func TestEncoding_String(t *testing.T) {
-	for e := Unknown; e <= UTF32LittleEndian; e++ {
+	for e := utfbom.Unknown; e <= utfbom.UTF32LittleEndian; e++ {
 		s := e.String()
 		if s == "" {
 			t.Errorf("no string for %#v", e)
 		}
 	}
-	s := Encoding(999).String()
+	s := utfbom.Encoding(999).String()
 	if s != "Unknown" {
 		t.Errorf("wrong string '%s' for invalid encoding", s)
 	}
